30bb0d40733818cd5f9264afb6b8c89e2ccdbd0d
Jinjerana <125341676+Jinjerana@users.noreply.github.com>
2023-09-26 22:15:09 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,34 @@
-export const App = () => {
-  return <div></div>;
-};
+import React, { Component } from 'react';
+
+export class App extends Component {
+  // Объявляем стейт
+  state = {
+    // Массив изображений
+    images: [],
+    // Поисковой запрос
+    query: '',
+    // Пагинация
+    page: 1,
+  };
+
+  render() {
+    const { images } = this.state;
+    return (
+      <>
+        {/* Searchbar */}
+        <form>
+          <label>
+            <input type="text" name="query" />
+          </label>
+          <button type="submit">Search</button>
+        </form>
+        {/* Gallery */}
+        <ul>
+          {images.map(item => (
+            <li key={item.id}>{item.largeImage}</li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
